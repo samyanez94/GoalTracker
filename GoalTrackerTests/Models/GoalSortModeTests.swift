fd4c8829f47c8d90b20c6bf4c8ff8afbee3baef2
Samuel Yanez <samuelyanez94@gmail.com>
2026-05-11 22:44:43 -0700

    Add GoalSortMode unit test

---
+++ GoalTrackerTests/Models/GoalSortModeTests.swift
@@ -0,0 +1,21 @@
+//
+//  GoalSortModeTests.swift
+//  GoalTrackerTests
+//
+//  Created by Codex on 5/12/26.
+//
+
+import Testing
+
+@testable import GoalTracker
+
+@Suite
+struct GoalSortModeTests {
+  @Test
+  func `titles are human readable`() {
+    #expect(GoalSortMode.manual.title == "Manual")
+    #expect(GoalSortMode.dueDate.title == "Due Date")
+    #expect(GoalSortMode.creationDate.title == "Date Created")
+    #expect(GoalSortMode.name.title == "Name")
+  }
+}
