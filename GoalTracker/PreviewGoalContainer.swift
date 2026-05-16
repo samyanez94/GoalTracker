5b8b22c7e29545c145a63e03b2f3e8817efcab8d
Samuel Yanez <samuelyanez94@gmail.com>
2026-05-16 12:56:22 -0700

    Fix preview container archive build

--- GoalTracker/PreviewGoalContainer.swift
+++ GoalTracker/PreviewGoalContainer.swift
@@ -5,22 +5,20 @@
 //  Created by Samuel Yanez on 5/14/26.
 //
 
-#if DEBUG
-    import SwiftData
+import SwiftData
 
-    @MainActor
-    enum GoalPreviewContainer {
-        static func make(goals: [Goal]) -> ModelContainer {
-            do {
-                let container = try GoalTrackerModelContainer.make(isStoredInMemoryOnly: true)
-                for goal in goals {
-                    container.mainContext.insert(goal)
-                }
-                try container.mainContext.save()
-                return container
-            } catch {
-                preconditionFailure("Failed to create preview model container: \(error)")
+@MainActor
+enum GoalPreviewContainer {
+    static func make(goals: [Goal]) -> ModelContainer {
+        do {
+            let container = try GoalTrackerModelContainer.make(isStoredInMemoryOnly: true)
+            for goal in goals {
+                container.mainContext.insert(goal)
             }
+            try container.mainContext.save()
+            return container
+        } catch {
+            preconditionFailure("Failed to create preview model container: \(error)")
         }
     }
-#endif
+}
